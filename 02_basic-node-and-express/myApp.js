3fc74357316d97cedf7c47d61984b879088e4d80
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-24 11:41:33 +0900

    Update to serve static assets to `/public`

--- 02_basic-node-and-express/myApp.js
+++ 02_basic-node-and-express/myApp.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+const assetsPath = __dirname + "/public";
+app.use("/public", express.static(assetsPath));
+
 app.get("/", (req, res) => {
   const filePath = __dirname + "/views/index.html";
   res.sendFile(filePath);
