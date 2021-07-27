dbc938563a13dec36df399f93ac48e59223c3aa2
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-27 22:39:32 +0900

    Implement `removeById` method

--- 03_mongodb-and-mongoose/myApp.js
+++ 03_mongodb-and-mongoose/myApp.js
@@ -101,7 +101,12 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, data);
+  });
 };
 
 const removeManyPeople = (done) => {
