249de66990bb139f0e7face92876c0b866d6dd03
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-27 21:57:22 +0900

    Implement `findOneByFood` method

--- 03_mongodb-and-mongoose/myApp.js
+++ 03_mongodb-and-mongoose/myApp.js
@@ -49,7 +49,12 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
