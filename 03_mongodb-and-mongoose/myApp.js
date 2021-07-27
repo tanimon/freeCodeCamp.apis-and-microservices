92c53aa90d9f9628a3c151285ab50e134e797b25
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-27 22:23:12 +0900

    Implement `findEditThenSave` method

--- 03_mongodb-and-mongoose/myApp.js
+++ 03_mongodb-and-mongoose/myApp.js
@@ -69,7 +69,19 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) {
+      return done(err);
+    }
+
+    data.favoriteFoods.push(foodToAdd);
+    data.save((err, data) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, data);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
