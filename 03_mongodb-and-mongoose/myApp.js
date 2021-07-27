0f077266397ff6f5d8a0a01c7b32b4a539a1ed46
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-27 21:45:24 +0900

    Implement `createManyPeople` method

--- 03_mongodb-and-mongoose/myApp.js
+++ 03_mongodb-and-mongoose/myApp.js
@@ -31,7 +31,12 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      return done(err);
+    }
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
