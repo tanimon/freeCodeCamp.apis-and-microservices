c3999ca0acf16dff7bd31f4599e5c19066945669
tanimon <8575113+tanimon@users.noreply.github.com>
2021-07-28 18:13:35 +0900

    Migrate from CommonJS module to ES6 module

--- 04_apis-and-microservices-projects/timestamp-microservice/server.js
+++ 04_apis-and-microservices-projects/timestamp-microservice/server.js
@@ -2,31 +2,33 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+import express from "express";
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+import cors from "cors";
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
